65648c1a9e8f595cac9244891b2e4f9ac125ce54
madrugado <madrugado@yandex.ru>
2013-03-06 19:51:31 +0400

    main is more useful now

--- mjpeg2jpeg.py
+++ mjpeg2jpeg.py
@@ -1,16 +1,18 @@
+#!/usr/bin/env python
+
 import sys
 
 
 class imageWriter:
     def __init__(self, filename):
         self.fileCounter = 0
-        self.baseFilename = filename
+        self.baseFilename = str(filename).split('.')[0]
 
     def writeBytes(self, bytesToWrite):
         if self.needToWrite is False:
             if bytesToWrite[0] == 0xFF & bytesToWrite[1] == 0xD8:
                 self.needToWrite = True
-                self.fileToWrite = open(self.baseFilename + str(self.fileCounter), "wb")
+                self.fileToWrite = open(self.baseFilename + str(self.fileCounter) + ".jpg", "wb")
                 self.fileToWrite.write(bytesToWrite)
         else:
             self.fileToWrite.write(bytesToWrite)
@@ -32,6 +34,9 @@ def bytesFromFile(filename, chunksize=2):
 
 
 def main(self):
+    if sys.argc < 2:
+        print "Usage: mjpeg2jpeg.py inputFile.mjpeg"
+        return 0
     w = imageWriter(sys.argv[1])
     for b in bytesFromFile(sys.argv[1]):
         w.writeBytes(b)
